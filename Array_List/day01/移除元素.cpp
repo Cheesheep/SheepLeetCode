121014176e298d6245d0b4ef6d8fb63703bb2c04
SHEEP_WIN_GIT01 <3438763668@qq.com>
2022-08-21 17:22:24 +0800

    22/8/21下午：提交移除元素（快慢指针）

--- Array_List/day01/移除元素.cpp
+++ Array_List/day01/移除元素.cpp
@@ -3,17 +3,29 @@
 
 using namespace std;
 
-class Solution {
+class Solution { //方法1.使用库函数
 public:
     int removeElement(vector<int>& nums, int val) {
         int length = nums.size();
-        vector<int>::iterator head = nums.begin();
-        for(int i = 0;i < nums.size();i++){
-            if(nums[i] == val){
-                nums.erase(head);
+        for(auto it = nums.begin();it != nums.end();){
+            if(*it == val){
+                nums.erase(it);
                 length--;
             }else
-                head++;    
+                it++;    
+        }
+        return length;
+    }
+    //方法2 ：快慢指针
+    int removeElement02(vector<int>& nums, int val) {
+        int length = nums.size();
+        int slow = 0;
+        for(int fast = 0;fast < nums.size();fast++){
+            if(nums[fast] != val)//当快指针不等于val时
+            {
+                nums[slow] = nums[fast];//慢指针先赋值
+                slow++;//然后再往后移一位，快指针也会移动，在for循环里面移动
+            }
         }
         return length;
     }
